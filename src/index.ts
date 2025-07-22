90efc11bdfb70b5d9a1fa200ed9a8710e30b18db
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2025-07-22 14:17:17 +0200

    Test uploading change

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 // Basic route
 app.get('/', (req: Request, res: Response<WelcomeResponse>) => {
   const response: WelcomeResponse = {
-    message: 'Welcome to the Simple Test Node Server! Now with GitHub Actions PRO EDITION!',
+    message: 'Welcome to the Simple Test Node Server! Now with GitHub Actions PRO MEGA EDITION!',
     timestamp: new Date().toISOString()
   };
   
