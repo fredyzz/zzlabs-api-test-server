6bff2203b9795d2f00f5dd656d80993261e56219
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2025-07-23 22:06:37 +0200

    CI change test

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 // Basic route
 app.get('/', (req: Request, res: Response<WelcomeResponse>) => {
   const response: WelcomeResponse = {
-    message: 'Welcome to the Simple Test Node Server! Now with GitHub Actions PRO MEGA EDITION 2!',
+    message: 'Welcome to the Simple Test Node Server! Now with GitHub Actions PRO MEGA EDITION 3!',
     timestamp: new Date().toISOString()
   };
   
